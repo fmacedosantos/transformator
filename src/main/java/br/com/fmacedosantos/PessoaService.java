ace7cb9dccf0b7fbe13aafe7e7bac9b92c6549ad
fmacedosantos <felipemacedo986@gmail.com>
2025-11-30 13:26:33 -0300

    Utilizando o transformator no service

--- src/main/java/br/com/fmacedosantos/PessoaService.java
+++ src/main/java/br/com/fmacedosantos/PessoaService.java
@@ -1,10 +1,21 @@
 package br.com.fmacedosantos;
 
+import br.com.fmacedosantos.reflection.Transformator;
+
+import java.lang.reflect.InvocationTargetException;
+
 public class PessoaService {
 
     public PessoaDTO list(){
         Pessoa pessoa = new PessoaRepository().list();
-        PessoaDTO pessoaDTO = new PessoaDTO(pessoa.getNome(), pessoa.getCpf());
+        PessoaDTO pessoaDTO = null;
+
+        try {
+            pessoaDTO = new Transformator().transform(pessoa);
+        } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | InstantiationException |
+                 IllegalAccessException e) {
+            e.printStackTrace();
+        }
 
         return pessoaDTO;
     }
