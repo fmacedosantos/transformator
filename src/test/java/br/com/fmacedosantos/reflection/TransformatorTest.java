a032bff3790ab099899fbad852107241dedfbd74
fmacedosantos <felipemacedo986@gmail.com>
2025-11-30 14:32:24 -0300

    Testando um cenário com a classe DTO faltando e melhorando nossos testes

--- src/test/java/br/com/fmacedosantos/reflection/TransformatorTest.java
+++ src/test/java/br/com/fmacedosantos/reflection/TransformatorTest.java
@@ -1,24 +1,44 @@
 package br.com.fmacedosantos.reflection;
 
+import br.com.fmacedosantos.Endereco;
 import br.com.fmacedosantos.Pessoa;
 import br.com.fmacedosantos.PessoaDTO;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.lang.reflect.InvocationTargetException;
 
 public class TransformatorTest {
 
     Pessoa pessoa = new Pessoa(1, "João", "1234");
+    Endereco endereco = new Endereco("Rua 10", 20);
+
+    Transformator transformator;
+
+    @BeforeEach
+    public void setUp(){
+        transformator = new Transformator();
+    }
 
     @Test
-    public void shouldTransform() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-        Transformator transformator = new Transformator();
-
+    @DisplayName("Should transform Entity to DTO ensuring type and field values match")
+    public void shouldTransformEntityToDtoWithMatchingValues() throws Exception {
+        // Act
         PessoaDTO pessoaDTO = transformator.transform(pessoa);
 
-        Assertions.assertInstanceOf(PessoaDTO.class, pessoaDTO);
-        Assertions.assertEquals(pessoaDTO.getNome(), pessoa.getNome());
-        Assertions.assertEquals(pessoaDTO.getCpf(), pessoa.getCpf());
+        // Assert
+        Assertions.assertAll("Verify DTO properties",
+                () -> Assertions.assertInstanceOf(PessoaDTO.class, pessoaDTO),
+                () -> Assertions.assertEquals(pessoa.getNome(), pessoaDTO.getNome()),
+                () -> Assertions.assertEquals(pessoa.getCpf(), pessoaDTO.getCpf())
+        );
+    }
+
+    @Test
+    @DisplayName("Should throw ClassNotFoundException when the target DTO class does not exist")
+    public void shouldThrowExceptionWhenDtoClassIsMissing(){
+        Assertions.assertThrows(ClassNotFoundException.class, () -> {
+            transformator.transform(endereco);
+        });
     }
 }
