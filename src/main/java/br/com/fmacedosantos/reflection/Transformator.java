41e9c24a24936dbafdb5e7a87f3ce1d9d0fe9b72
fmacedosantos <felipemacedo986@gmail.com>
2025-11-29 21:29:27 -0300

    Adicionando o retorno do método transform

--- src/main/java/br/com/fmacedosantos/reflection/Transformator.java
+++ src/main/java/br/com/fmacedosantos/reflection/Transformator.java
@@ -2,7 +2,7 @@ package br.com.fmacedosantos.reflection;
 
 public class Transformator {
 
-    public <I, O> transform(I input) throws ClassNotFoundException {
+    public <I, O> O transform(I input) throws ClassNotFoundException {
         Class<?> source = input.getClass();
         Class<?> target = Class.forName(source.getName() + "DTO");
     }
