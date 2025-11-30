3fa66213435d941163691c1f21eb471a8d224cef
fmacedosantos <felipemacedo986@gmail.com>
2025-11-29 21:32:04 -0300

    Instanciando a classe DTO genérica

--- src/main/java/br/com/fmacedosantos/reflection/Transformator.java
+++ src/main/java/br/com/fmacedosantos/reflection/Transformator.java
@@ -1,9 +1,15 @@
 package br.com.fmacedosantos.reflection;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class Transformator {
 
-    public <I, O> O transform(I input) throws ClassNotFoundException {
+    public <I, O> O transform(I input) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         Class<?> source = input.getClass();
         Class<?> target = Class.forName(source.getName() + "DTO");
+
+        O targetClass = (O) target.getDeclaredConstructor().newInstance();
+
+        return targetClass;
     }
 }
