410de1721da96fe03ce4e6a76ffc7d3ee5bedb73
fmacedosantos <felipemacedo986@gmail.com>
2025-11-29 21:02:16 -0300

    Criando o cenário inicial com DTO feito na mão - Mock

---
+++ src/main/java/br/com/fmacedosantos/PessoaDTO.java
@@ -0,0 +1,12 @@
+package br.com.fmacedosantos;
+
+public class PessoaDTO {
+
+    private String nome;
+    private String cpf;
+
+    public PessoaDTO(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+}
---
+++ src/main/java/br/com/fmacedosantos/PessoaService.java
@@ -0,0 +1,11 @@
+package br.com.fmacedosantos;
+
+public class PessoaService {
+
+    public PessoaDTO list(){
+        Pessoa pessoa = new PessoaRepository().list();
+        PessoaDTO pessoaDTO = new PessoaDTO(pessoa.getNome(),pessoa.getCpf());
+
+        return pessoaDTO;
+    }
+}
